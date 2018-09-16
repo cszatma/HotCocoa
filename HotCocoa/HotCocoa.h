1e1aaf70387d67998fc6f41852ebf53f539791e4
Christopher Szatmary <cs@christopherszatmary.com>
2018-09-16 13:28:41 -0400

    Initial Commit

---
+++ HotCocoa/HotCocoa.h
@@ -0,0 +1,19 @@
+//
+//  HotCocoa.h
+//  HotCocoa
+//
+//  Created by Christopher Szatmary on 2018-09-16.
+//  Copyright © 2018 Christopher Szatmary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HotCocoa.
+FOUNDATION_EXPORT double HotCocoaVersionNumber;
+
+//! Project version string for HotCocoa.
+FOUNDATION_EXPORT const unsigned char HotCocoaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HotCocoa/PublicHeader.h>
+
+
